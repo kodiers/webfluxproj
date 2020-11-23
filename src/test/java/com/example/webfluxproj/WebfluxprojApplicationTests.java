7f7fdb006e3c20682c026c7c717779370d143da2
Victor Yamchinov <kodiers@gmail.com>
2020-11-23 21:03:52 +0300

    Initial commit

---
+++ src/test/java/com/example/webfluxproj/WebfluxprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
